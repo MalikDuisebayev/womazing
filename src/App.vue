4f16279b65c78af64c0c9e718a0b96b2ca8cabe7
Malik Duisebayev <malik.duisebaev@mail.ru>
2022-06-08 21:05:13 +0600

    init: create project womazing

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <navbar />
+    <div class="container app">
+      <router-view></router-view>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Navbar from './components/UI/Navbar.vue'
+export default {
+  components: { Navbar }
+
+}
+</script>
+
+<style>
+</style>
